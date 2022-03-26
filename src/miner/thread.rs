c7135b38ba3267b0a440cd1ad6c75300eb007983
Andrew Chen <andrewchen0028@gmail.com>
2022-03-25 20:52:57 -0400

    make miner thread print to stdout every second

--- src/miner/thread.rs
+++ src/miner/thread.rs
@@ -1,13 +1,12 @@
-use std::io::Result;
+use std::{io::Result, thread, time::Duration};
 
 use crate::util::networking::addr_from_node_number;
 
 /// Start mining.
 pub fn start_miner_thread(node_number: &u8) -> Result<()> {
   let addr = addr_from_node_number(*node_number);
-  println!(
-    "Constructed addr from node number in miner thread:\t{}",
-    addr
-  );
-  Ok(())
+  loop {
+    println!("Mining on address {}...", &addr);
+    thread::sleep(Duration::from_secs(1));
+  }
 }
