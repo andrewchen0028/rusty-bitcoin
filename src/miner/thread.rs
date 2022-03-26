7e9f2a751373b8c635873267846b8e877ffe1ea4
Andrew Chen <andrewchen0028@gmail.com>
2022-03-26 01:44:50 -0400

    add bootstrap node constants

--- src/miner/thread.rs
+++ src/miner/thread.rs
@@ -4,9 +4,10 @@ use crate::util::networking::addr_from_node_number;
 
 /// Start mining.
 pub fn start_miner_thread(node_number: &u8) -> Result<()> {
+  // Get local address from provided node number.
   let addr = addr_from_node_number(*node_number);
   loop {
+    thread::sleep(Duration::from_secs(5));
     println!("Mining on address {}...", &addr);
-    thread::sleep(Duration::from_secs(1));
   }
 }
--- src/util/constants.rs
+++ src/util/constants.rs
@@ -3,3 +3,13 @@ pub const RBTC_PORT: u16 = 42069;
 
 /// The number of bytes that make up a Rusty Bitcoin message.
 pub const MSG_SIZE: usize = 1;
+
+/// The number of bootstrap nodes in the Rusty Bitcoin network.
+/// NOTE: BOOTSTRAP_NODE_COUNT should really be a `u8` for IPv4 addresses, but
+/// it needs to be a `usize` to work as an array length.
+pub const BOOTSTRAP_NODE_COUNT: usize = 2;
+
+/// The list of boostrap node addresses in the Rusty Bitcoin network.
+/// NOTE: BOOTSTRAP_NODE_COUNT should really be a `u8` for IPv4 addresses, but
+/// it needs to be a `usize` to work as an array length.
+pub const BOOTSTRAP_NODE_NUMBERS: [u8; BOOTSTRAP_NODE_COUNT] = [2, 3];
